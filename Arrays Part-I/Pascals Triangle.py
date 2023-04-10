b184808d982dc5c67394910b1317170306299ae6
Samarth Patel <psamarth8836@gmail.com>
2023-04-10 19:34:01 -0400

    Pascal's Triangle Memoized

--- Arrays Part-I/Pascals Triangle.py
+++ Arrays Part-I/Pascals Triangle.py
@@ -1,25 +1,27 @@
 # Given an integer N, return the first N rows of Pascal’s triangle
 
-def pascalsTriangle(n):
-    if n == 2:
-        return [1, 1]
-    elif n == 1:
-        return [1]
-
-    lastRow = pascalsTriangle(n-1)
+def pascalsTriangle(storage):
+    if not len(storage):
+        storage.append([1])
+        return storage
+    
+    lastRow = storage[-1]
     newRow = [1 for i in range(len(lastRow) + 1)]
     for i in range(1, len(lastRow)):
         newRow[i] = lastRow[i-1] + lastRow[i]
-    return newRow
+    
+    storage.append(newRow)
+    return storage
 
 
 if __name__ == '__main__':
     n = 6
+    storage = []
     for i in range(n):
-        values = pascalsTriangle(i+1)
+        storage = pascalsTriangle(storage)
         for j in range(n-1-i):
             print(" ", end='')
-        for j in values:
+        for j in storage[-1]:
             print(j, end=' ')
         print()
   
